08c60d4a7728ea26d8be29a7895fe0720960d776
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2016-10-06 11:54:25 +0100

    - WIP

--- surveymonkey/test.js
+++ surveymonkey/test.js
@@ -6,18 +6,18 @@ const surveyData = require('./data/survey');
 const responseData = require('./data/responses');
 
 let getQuestions = function (collection){
-    return _.map(collection, function(value){
+    return _.flatMap(collection, function(value){
         return value.questions;
     });
 };
 
 let questionsList = getQuestions(surveyData.pages);
 //console.log(questionsList);
-let filterQuestions = _.flatMap(questionsList, function(question){
+let filterQuestions = _.map(questionsList, function(question){
     //console.log(question)
     return {
         qid: question.id,
-        label: question.title,
+        label: question.heading,
         answers: question.answers
     }
 });
