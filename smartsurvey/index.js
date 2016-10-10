e3630f932399f7ab40035d1672ca5862f4e6af4d
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2016-10-10 17:21:28 +0100

    smartSurvey: get all ids no matter the number of pages

--- smartsurvey/index.js
+++ smartsurvey/index.js
@@ -1,42 +1,66 @@
-var request = require('request');
-var _ = require('lodash');
+"use strict";
 
-//request('http://www.bing.com', function (error, response, body) {
-//   if (!error && response.statusCode === 200) {
-//       console.log(body);
-//   }
-//});
+let request = require('request');
+let _ = require('lodash');
+let results = [];
+let pageSize = 10;
+let pageTotal;
+let noPages;
+let pageNumber = 1;
 
-var options = {
-    url: 'https://api.smartsurvey.io/v1/surveys/268842/responses',
-    qs: {
-        'api_token': process.env.API_TOKEN,
-        'api_token_secret': process.env.TOKEN_SECRET,
-        'completed': 2,
-        'include_labels': 'true',
-        'page_size': 5,
-        'page':1
-    },
-    headers: {
-        'User-Agent': 'request'
+let getOptions = function getOptions(currentPage) {
+    if(!currentPage) {
+        currentPage = 1;
     }
+
+    return {
+        url: 'https://api.smartsurvey.io/v1/surveys/268842/responses',
+        qs: {
+            'api_token': process.env.API_TOKEN,
+            'api_token_secret': process.env.TOKEN_SECRET,
+            'completed': 2,
+            'include_labels': 'true',
+            'page_size': pageSize,
+            'page':currentPage
+        },
+        headers: {
+            'User-Agent': 'request'
+        }
+    };
 };
-var results = [];
+
 
 function callback(error, response, body) {
 
     if (!error && response.statusCode == 200) {
         var info = JSON.parse(body);
-
-        console.log(JSON.stringify(response.headers));
-
         _.forEach(info, function(value) {
             results.push(value.id);
         });
 
     }
 
-    console.log(results);
+    pageTotal = response.headers['x-ss-pagination-total'];
+    noPages = getNoPages(pageTotal, pageSize);
+
+    if (pageNumber <= noPages) {
+        pageNumber = pageNumber + 1;
+        request(getOptions(pageNumber), callback);
+    }
+    else {
+        console.log(results.length)
+    }
 }
 
-request(options, callback);
+function getNoPages(pageTotal, pageSize){
+    let val =  Math.floor(pageTotal / pageSize);
+
+    if ((pageTotal % pageSize) > 0) {
+        val++;
+    }
+    return val;
+
+}
+
+
+request(getOptions(pageNumber), callback);
