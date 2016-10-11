3036de760b9f545c4833ad84d695445ad548a9f2
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2016-10-11 16:10:44 +0100

    smartSurvey: get all ids no matter the number of pages (#4)
    * smartSurvey: get all ids no matter the number of pages
    * delete useless file

---
+++ surveymonkey/test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const _ = require('lodash');
+
+const surveyData = require('./data/survey');
+const responseData = require('./data/responses');
+
+let getQuestions = function (pages){
+    return _.flatMap(pages, function(page){
+        return page.questions;
+    });
+};
+
+let questionsList = getQuestions(surveyData.pages);
+
+let filterQuestions = _.map(questionsList, function(question){
+    return {
+        qid: question.id,
+        label: (question.headings),
+        answers: (question.answers.choices)
+    }
+});
+
+console.log(`filter questions ${JSON.stringify(filterQuestions, null, 2)}`);
